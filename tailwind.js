7795293cf98d1b5d2f275f7960cb793298addec3
Amie Chen <chinghc@gmail.com>
2018-07-04 17:36:47 +0800

    add tabs template

--- tailwind.js
+++ tailwind.js
@@ -805,7 +805,7 @@ module.exports = {
     borderRadius: ["responsive"],
     borderStyle: ["responsive"],
     borderWidths: ["responsive", "hover"],
-    cursor: ["responsive"],
+    cursor: ["responsive", "hover"],
     display: ["responsive"],
     flexbox: ["responsive"],
     float: ["responsive"],
