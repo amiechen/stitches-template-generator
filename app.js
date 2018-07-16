ef1232ec19d25ce4e96f4811233a8f9ae3753ef0
Amie Chen <chinghc@gmail.com>
2018-07-16 23:14:51 +0800

    replace avenir with muli

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const http = require("http");
 const stitchesCSSPath =
-  "https://rawgit.com/amiechen/web-component-library/master/public/stitches.css";
+  "https://cdn.rawgit.com/amiechen/stitches-template-generator/master/public/stitches.css";
 const fontMuliPath = "https://fonts.googleapis.com/css?family=Muli:300,400,600,700,800,900";
 const fontAwesomePath = "https://use.fontawesome.com/releases/v5.0.13/css/all.css";
 const fontAwesomeIntegrity = "sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp";
