c186f5b6126962a8a83df71e0d62cbbda1d14f44
May Kittens Devour Your Soul <yoshimitsu002@gmail.com>
2018-12-30 17:11:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const http = require("http");
 const stitchesCSSPath =
   "https://cdn.rawgit.com/amiechen/stitches-template-generator/master/public/stitches.css";
 const fontMuliPath = "https://fonts.googleapis.com/css?family=Muli:300,400,600,700,800,900";
-const fontAwesomePath = "https://use.fontawesome.com/releases/v5.0.13/css/all.css";
-const fontAwesomeIntegrity = "sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp";
+const fontAwesomePath = "https://use.fontawesome.com/releases/v5.6.3/css/all.css";
+const fontAwesomeIntegrity = "sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/";
 
 let stitchesHTML = html => `<html lang="en">
   <head>
